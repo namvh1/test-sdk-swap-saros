8bec8c9fb1f4b28dc5b4cfc1de4910f49c413487
namvh <namvh@coin98.finance>
2025-09-04 10:56:02 +0700

    fix: update import path for swap functions in TestAMM component

--- src/app/TestAMM.tsx
+++ src/app/TestAMM.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { getSwapExactOutSaros, swapSaros } from "../saros-sdk-clone/src/swap";
+import { getSwapExactOutSaros, swapSaros } from "../sdk/src/swap";
 import React, { useState } from "react";
 import { Connection, PublicKey } from "@solana/web3.js";
 
